cb2e00363fd8bfd9a2f2cd1ebbc14aab00cfa3be
Nikhil Kini <nkini@ucsc.edu>
2017-04-15 16:33:14 -0700

    First implementation of standard blocking. Highly unoptimized, and likely will not work with millions or billions of records

---
+++ standard_blocking.py
@@ -0,0 +1,89 @@
+import json
+import numpy as np
+from collections import defaultdict
+from itertools import combinations
+
+def standard_blocking(data, blocking_key):
+    '''
+    data: dict ({
+                <reference_id>: dict ({
+                                'features': dict ({...}),
+                                'entity_id': ...
+                            })
+            })
+            
+    blocking_key: key such that data[<reference_id>]['features'][<blocking_key>]
+            returns an iterable with values to block on.
+    '''
+    num_references = len(data)
+    ent_ref_map = defaultdict(set)
+    
+    # create blocks and ground truth
+    blocks = defaultdict(set)
+    for refid, refdata in data.items():
+        ent_ref_map[refdata['entity_id']].add(refid)
+        for it in data[refid]['features'][blocking_key]:
+            blocks[it].add(refid)
+    print('created blocks. Number of blocks:',len(blocks))
+            
+    ent_ref_ground_truth = set()
+    for entid, refs in ent_ref_map.items():
+        for pair in combinations(refs, 2):
+            ent_ref_ground_truth.add(pair)
+    print('created ground truth')
+            
+    print('\nBlocking on %s\n' % blocking_key)
+    
+    # calculate num pairs to compare post blocking
+    pairs_after_blocking = set()
+    for blockid, block in blocks.items():
+        for pair in combinations(block, 2):
+            pairs_after_blocking.add(pair)
+    print('calculated number of pairs after blocking:',len(pairs_after_blocking))
+
+    # Calculate reduction ratio
+    num_comp_before_blocking = (num_references*(num_references - 1))/2
+    num_comp_after_blocking = len(pairs_after_blocking)
+    print('Num comparisons without blocking: %d' % num_comp_before_blocking)
+    print('Num comparisons after blocking: %d' % num_comp_after_blocking)
+    print('Reduction ratio: %f' % (1 - num_comp_after_blocking/num_comp_before_blocking))
+    
+    # Calculate recall
+    recall = len(pairs_after_blocking & ent_ref_ground_truth)/len(ent_ref_ground_truth)
+    print('Recall: %f' % recall)
+    print('\n')
+
+def test_standard_blocking(blocking_key):
+    '''
+    data: dict ({
+                <reference_id>: dict ({
+                                'features': dict ({...}),
+                                'entity_id': ...
+                            })
+            })
+    '''
+    data = {
+        'r1' : {
+            'features' : { 'region' : set([1, 5, 6]), 'ipaddress': set([12, 45, 65]), 'ua': set([1])},
+            'entity_id': 2381374681
+        },
+        'r2' : {
+            'features' : { 'region' : set([5, 6]), 'ipaddress': set([12, 45, 65]), 'ua': set([2])},
+            'entity_id': 2381374682
+        },
+        'r3' : {
+            'features' : { 'region' : set([1, 3, 7]), 'ipaddress': set([12, 45, 65]), 'ua': set([1])},
+            'entity_id': 2381374681
+        },
+        'r4' : {
+            'features' : { 'region' : set([1, 3, 7]), 'ipaddress': set([12, 45, 65]), 'ua': set([2])},
+            'entity_id': 2381374682
+        }
+    }
+     
+    standard_blocking(data, blocking_key)
+
+if __name__  == '__main__':    
+    test_standard_blocking('region')
+    test_standard_blocking('ipaddress')
+    test_standard_blocking('ua')
