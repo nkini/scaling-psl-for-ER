161633033034b3266f404b24055af0255d0d4efa
Nikhil Kini <nkini@ucsc.edu>
2017-05-15 14:34:42 -0700

    Sticking to Spark 2.0.0

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-#mvn compile
-#mvn dependency:build-classpath -Dmdep.outputFile=classpath.out
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+mvn compile
+mvn dependency:build-classpath -Dmdep.outputFile=classpath.out
 java -cp ./target/classes:`cat classpath.out` org.linqs.psladmmspark.PSLMaster
